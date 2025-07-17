7580ea23e637c73e0b1426bc9fd2cdc8ddf87106
AbhimสŇyu <98833613+kameno22@users.noreply.github.com>
2025-07-17 18:08:20 +0530

    updated

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const icon = document.getElementById('theme-icon');
 
   // Update these if your files are elsewhere or have other names
-  const LIGHT_ICON = 'theme-light.png';
-  const DARK_ICON  = 'theme-dark.png';
+  const LIGHT_ICON = 'https://raw.githubusercontent.com/kameno22/nexus/refs/heads/main/resources/theme-light.png';
+  const DARK_ICON  = 'https://raw.githubusercontent.com/kameno22/nexus/refs/heads/main/resources/theme-dark.png';
 
   function setTheme(theme) {
     html.setAttribute('data-theme', theme);
